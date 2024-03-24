95f954f4f0b6983cbdd7b0b7f1bd97000f01661b
Flávia Reis <flaviarster@gmail.com>
2024-03-24 16:26:38 -0300

    I added the custom colors and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'dm': ['DM Sans', 'sans-serif'],
+      },
+      colors: {
+        soft_red: 'hsl(10, 79%, 65%)',
+        cyan: 'hsl(186, 34%, 60%)',
+        dark_brown: 'hsl(25, 47%, 15%)',
+        medium_brown: 'hsl(28, 10%, 53%)',
+        cream: 'hsl(27, 66%, 92%)',
+        very_pale_orange: 'hsl(33, 100%, 98%)',
+      },
+    },
   },
   plugins: [],
 }
